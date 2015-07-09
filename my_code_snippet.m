7f60a306d0a98d428c9043723d2f1a718110450a
mac <mac@macwifi.local>
2015-07-10 00:31:35 +0800

    Imported user code snippets

---
+++ my_code_snippet.m
@@ -0,0 +1,22 @@
+// My Code Snippet
+// 
+//
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 8A89E1A9-F471-4C55-B88C-0DD30299CFD8
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+
+
+
+(self.partner) {
+        [discription appendFormat:@"partner=\"%@\"", self.partner];
+    }
+	
+    if (self.seller) {
+        [discription appendFormat:@"&seller_id=\"%@\"", self.seller];
+    }
+	if (self.tradeNO) {
+        [discription appendFormat:@"&out_trade_no=\"%@\"", self.tradeNO];
+    }
+	if (self.productName) {
