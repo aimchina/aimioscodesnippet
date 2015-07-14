18c5f0836b170dca7fb9f6da100b2f1988d7cf7c
reinventgithub <reinventgithub@yeah.net>
2015-07-14 19:09:49 +0800

    Move To "wxl"

--- wxl/@property-assign.m
+++ wxl/@property-assign.m
@@ -6,5 +6,5 @@
 // IDECodeSnippetIdentifier: 75A20B64-0B0B-4409-AF3E-86283752FE8D
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 @property (assign, nonatomic) <#type#> <#name#>;
