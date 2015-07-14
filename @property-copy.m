e86c4fca151af6a743fbfe93d8a742d27ba9dfc2
reinventgithub <reinventgithub@yeah.net>
2015-07-14 18:41:40 +0800

    Imported user code snippets

---
+++ @property-copy.m
@@ -0,0 +1,10 @@
+// @property-copy
+// 
+//
+// IDECodeSnippetCompletionPrefix: copy
+// IDECodeSnippetCompletionScopes: [ClassInterfaceMethods]
+// IDECodeSnippetIdentifier: 901AFF5B-748C-4BEE-B9AD-E9684A9136B8
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+@property (copy, nonatomic) <#type#> * <#name#>
